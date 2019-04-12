fa5d456af6982a79096f1a406fabefa2a3ca92d6
Dan Walker <danrwalker1@gmail.com>
2019-04-12 01:10:02 +0100

    Correct the email protocol hook

--- dist/phpmailer/install.php
+++ dist/phpmailer/install.php
@@ -6,4 +6,4 @@
 	Twist::framework()->package()->importSettings(sprintf('%s/Data/settings.json',dirname(__FILE__)));
 
 	//Add a new email send protocol to the system
-	\Twist::framework()->hooks()->register('TWIST_EMAIL_PROTOCOLS','phpmailer-email-protocol','Packages\phpmailer\Models\Send',true);
+	\Twist::framework()->hooks()->register('TWIST_EMAIL_PROTOCOLS','phpmailer',array('model' => 'Packages\phpmailer\Models\Send'),true);
